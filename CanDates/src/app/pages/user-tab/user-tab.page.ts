4ac5b97a4519e131167049cb9ff2c3d421ef2dfb
javierpersan <61273246+javierpersan@users.noreply.github.com>
2023-11-09 09:46:16 +0100

    .

--- CanDates/src/app/pages/user-tab/user-tab.page.ts
+++ CanDates/src/app/pages/user-tab/user-tab.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { gsap } from 'gsap';
+
 
 @Component({
   selector: 'app-user-tab',
@@ -10,23 +10,13 @@ export class UserTabPage implements OnInit {
 
   constructor() { }
   ionViewWillEnter() {
-    this.animateTab();
+   
   }
   
   ngOnInit() {
-    this.animateTab();
+    
 
   }
-  animateTab() {
-    const tabContent = document.querySelector('.content-class'); // Asegúrate de seleccionar el elemento correcto.
-    gsap.from(tabContent, {
-      x: '-100%',
-      duration: 1,
-      ease: 'power3.out',
-      stagger: {
-        amount: 0.3
-      }
-    });
-  }
+  
 
 }
