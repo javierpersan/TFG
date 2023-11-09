7523ae4f5b0e4d18e4a2be3d4c6dfd5fd01cb3f7
javierpersan <61273246+javierpersan@users.noreply.github.com>
2023-11-09 11:08:29 +0100

    bien profile page

--- CanDates/src/app/pages/user-tab/user-tab.page.ts
+++ CanDates/src/app/pages/user-tab/user-tab.page.ts
@@ -1,22 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
-
+import { IonContent } from '@ionic/angular';
+import { ViewChild } from '@angular/core';
 @Component({
   selector: 'app-user-tab',
   templateUrl: './user-tab.page.html',
   styleUrls: ['./user-tab.page.scss'],
 })
 export class UserTabPage implements OnInit {
-
+  
   constructor() { }
-  ionViewWillEnter() {
-   
-  }
   
   ngOnInit() {
     
 
   }
   
-
 }
